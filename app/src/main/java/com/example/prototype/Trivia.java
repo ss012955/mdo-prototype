4d6f984d1fdde64aedeb31b67ec318d41bd4e5d3
ss012955 <153585604+ss012955@users.noreply.github.com>
2024-11-27 15:34:35 +0800

    trivia ui done

--- app/src/main/java/com/example/prototype/Trivia.java
+++ app/src/main/java/com/example/prototype/Trivia.java
@@ -1,8 +1,12 @@
 package com.example.prototype;
 
+import static java.security.AccessController.getContext;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +21,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.tabs.TabLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,7 +36,8 @@ public class Trivia extends BaseActivity implements ItemClickListener {
     private RecyclerView recyclerView;
     private TriviaAdapter triviaAdapter;
     private List<TriviaItem> triviaList = new ArrayList<>();
-
+    TabLayout tabLayout;
+    private ImageView chatImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,9 +46,35 @@ public class Trivia extends BaseActivity implements ItemClickListener {
         recyclerView = findViewById(R.id.recyclerViewTrivia);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        chatImageView = findViewById(R.id.chat);
+
+        // Set click listener for the chat image
+        chatImageView.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+
         triviaAdapter = new TriviaAdapter(triviaList);
         recyclerView.setAdapter(triviaAdapter);
+        tabLayout = findViewById(R.id.tablayout);
+        int[] icons = {R.drawable.home, R.drawable.user_journal, R.drawable.profile};
+        for (int i = 0; i < icons.length; i++) {
+            tabLayout.addTab(tabLayout.newTab().setIcon(icons[i]));
+        }
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                startActivity(new Intent(Trivia.this, home.class)
+                        .putExtra("tab_position", tab.getPosition()));
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
 
     }
 
